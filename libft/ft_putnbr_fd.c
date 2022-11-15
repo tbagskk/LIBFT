caa1ca36bf6a70e08d6cb13b6f636b3780b5a8a8
Gabin <87612170+tbagskk@users.noreply.github.com>
2022-11-15 11:30:14 +0100

    Add files via upload

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,15 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcherqui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/15 11:27:04 by gcherqui          #+#    #+#             */
-/*   Updated: 2022/11/15 11:27:28 by gcherqui         ###   ########.fr       */
+/*   Created: 2022/11/15 11:20:02 by gcherqui          #+#    #+#             */
+/*   Updated: 2022/11/15 11:23:16 by gcherqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nbr;
 
+	if (n < 0)
+	{
+		nbr = -n;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nbr = n;
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((nbr % 10) + '0', fd);
+}
